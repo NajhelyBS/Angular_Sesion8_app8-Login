a89cfe11d8759493ddd9b743873c941569af596d
NajhelyBS <s5anbandasanchez@gmail.com>
2022-12-30 09:45:35 -0500

    Trying to understand the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app8';
+  usuario = "Angular";
+  password = "123456";
 }
--- src/app/condition.directive.ts
+++ src/app/condition.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appCondition]'
 })
 export class ConditionDirective {
-
+     
   constructor() { }
 
 }
